383ba8b5cd6e0418266206be4b0b770a4b178630
Zach Simon <zbsimon@cs.washington.edu>
2015-12-09 20:38:35 -0800

    graphing & sd card logging working

--- Cardio1/Cardio1.ino
+++ Cardio1/Cardio1.ino
@@ -8,7 +8,7 @@
 #include <SdFat.h> // enable reading SD card on TFT
 #include "ILI9341_t3.h"
 
-#define SD_CS 21
+#define SD_CS 4
 
 // samle rate in hz
 #define SAMPLE_RATE 250
@@ -26,29 +26,38 @@ static const uint8_t channel2sc1a[] = {
 #define PDB_CH0C1_TOS 0x0100
 #define PDB_CH0C1_EN 0x01
 
-
 char *filename_template = "ecg_log_data_%d.csv";
 char *header_template = "zsfg:%d at %d Hx\n";
 char *log_line_template = "%d, %d, %d, %d, %d, %d, %d, %d\n";
 char *eof = "EOF";
 
-// buffer to store ADC output during dma
-
-
 // We choke when it's this large, but I think we need this much
 // space!
-// #define NUM_DMA_SAMPLES 320
 #define NUM_DMA_SAMPLES 16
+#define NUM_SAMPLES (30 * SAMPLE_RATE)
 
-#define THIRTY_SECONDS_OF_SAMPLES (30 * SAMPLE_RATE)
+uint16_t dma_buffer[NUM_DMA_SAMPLES]; // Buffer used to store ADC data
+uint16_t samples[NUM_SAMPLES]; // Buffer that stores 30 seconds of data
+uint16_t* buff_front = samples; // Pointer to where we are writing
+uint16_t* baseRead = samples; // Pointer to the beginning of the chunk we are drawing
+uint16_t* readIndex = samples; // Pointer to the part of the chunk we are drawing
 
-uint16_t dma_buffer[NUM_DMA_SAMPLES];
-uint16_t samples[THIRTY_SECONDS_OF_SAMPLES];
-uint16_t *buff_front = samples;
+const int SAMPLES_TO_DRAW = 320;
+const int BASELINE = 2000;
+const int MAX = 4095;
+const int HALF_MAX = 2048;
+const int MIN = 0;
+const int GRID_SIZE = 20;
+const int SCREEN_HEIGHT = 240;
+const int SCREEN_WIDTH = 320;
+const int NUM_HLINES = SCREEN_WIDTH / GRID_SIZE;
+int HLineHeights[NUM_HLINES];
+bool VLinePositions[SCREEN_WIDTH];
+bool stabilized = false;
+bool writeToSDCard = false;
 
 // Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
 ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
-
 
 SdFat sd;  // fs obj
 
@@ -58,59 +67,59 @@ const int max_height = 240;
 bool dma_occurred = false;  // flag to know in loop when we recieved fresh data
 
 void setup() {
-  for (int i = 0; i < NUM_DMA_SAMPLES; i++) {
-    samples[i] = 0;
-  }
-
-  // setup lcd
-  tft.begin(); // Initialize the display
-  tft.setRotation(1); //  0 or 2) width = width, 1 or 3) width = height, swapped etc
-
-  // I don't think this is actually doing what we want
-  // analogReadResolution(12);
-
+  // Initialize Pins
   pinMode(ecg_input_pin, INPUT);
 
+  // Initialize Serial and wait for Serial
   Serial.begin(9600);
-  while (!Serial); // wait until the serial debug window is opened
+  // while (!Serial);
 
-  adcInit();
+  // Initialize PDB/ADC/DMA
   pdbInit();
+  adcInit();
   dmaInit();
 
+  // Intialize LCD and Graph
+  reset_data();
   initialize_lcd_graph();
+  display_stabilization_screen();
 
+  // Initialize SD Card
   initialize_sd_card();
-
-  if (!sd.begin(SD_CS, SPI_HALF_SPEED)) sd.initErrorHalt();
-
 }
 
 void loop() {
+  if (stabilized)
+    drawColumn();
 
   // need to wait for data to stabilize?
   // neet to wait for button to be pressed?
   if (dma_occurred) {
     dma_occurred = false;
-    /* Serial.print("SAMPLES:"); */
-    /* for (int i = 0; i < NUM_SAMPLES; i++) { */
-    /*   Serial.printf(" %d ", samples[i]); */
-    /* } */
-    /* Serial.println(); */
+    beginWriteToSDCard();
+  }
+}
 
-    update_lcd_graph();
+// TODO
+void initialize_sd_card() {
+  if (!sd.begin(SD_CS, SPI_HALF_SPEED)) sd.initErrorHalt();
+}
 
-
+void beginWriteToSDCard() {
     // If we've collected 30 seconds of data, write to sd card
     // note that we don't even need to be collecting data at this point
-    if (buff_front == samples + THIRTY_SECONDS_OF_SAMPLES) {
+    if (writeToSDCard) {
+      writeToSDCard = false;
+      Serial.println("Writing to SD CArd");
       NVIC_DISABLE_IRQ(IRQ_DMA_CH1);
       char header[30];
       char filename[30];
       sprintf(header, header_template, hourFormat12(), SAMPLE_RATE);
       sprintf(filename, filename_template, hourFormat12());
+      Serial.println(header);
+      Serial.println(filename);
       SdFile data_file;  // log file
-      if (!data_file.open(filename, O_WRITE | O_READ)) {
+      if (!data_file.open(filename, O_WRITE | O_READ | O_CREAT)) {
         sd.errorHalt("opening log file failed");
         return;
       }
@@ -119,17 +128,18 @@ void loop() {
       // 8 samples, we do the last line outside of the loop.
       uint16_t *sample;
       for (sample = samples;
-           sample < samples + THIRTY_SECONDS_OF_SAMPLES - 8;
+           sample < samples + NUM_SAMPLES - 8;
            sample +=8) {
         char log_line[100];
         sprintf(log_line, log_line_template, sample, sample + 1, sample + 2,
                 sample + 3, sample + 4, sample + 5, sample + 6, sample + 7);
         data_file.write(log_line);
       }
-      while (sample < samples + THIRTY_SECONDS_OF_SAMPLES - 1) {
+      while (sample < samples + NUM_SAMPLES - 1) {
         char * templ = "%d, ";
         char sample_text[10];
         sprintf(sample_text, templ, sample);
+        Serial.println(sample_text);
         data_file.write(sample_text);
         sample++;
       }
@@ -137,42 +147,94 @@ void loop() {
       char sample_text[10];
       sprintf(sample_text, templ, sample);
       data_file.write(sample_text);
+      Serial.println(sample_text);
       data_file.write(eof);
       data_file.close();
       NVIC_ENABLE_IRQ(IRQ_DMA_CH1);
     }
-
-  }
-
 }
 
-// TODO
-void initialize_sd_card() {
-
-}
-
-// TODO
+// Perform initial setup
 void initialize_lcd_graph() {
-  // draw lines
-
-  // 1
-
-}
-
-
-
-void update_lcd_graph() {
+  tft.begin();
   tft.fillScreen(ILI9341_BLACK);
-  for (int i = 0; i < NUM_DMA_SAMPLES; i++) {
-    int val = (int)(((float) samples[i]) / 4096 * max_height);
-    tft.drawPixel(i, val, ILI9341_WHITE);
+  tft.setRotation(2);
+
+  // Calculate Line positions for horizontal lines
+  for (int i = 1; i < NUM_HLINES - 1; i++) {
+    HLineHeights[i] = i * GRID_SIZE;
   }
-  /* for (int i = 0; i < NUM_SAMPLES - 1; i++) { */
-  /*   int val = (int)(((float) samples[i]) / 4096 * max_height); */
-  /*   tft.drawLine(i, val, i+1, ILI9341_WHITE); */
-  /* } */
+
+  // Calculate Line positions for vertical lines
+  for (int i = 1; i < 320; i++) {
+    VLinePositions[i] = (i % GRID_SIZE) == 0;
+  }
 }
 
+void reset_data() {
+  for (int i = 0; i < NUM_DMA_SAMPLES; i++) {
+    samples[i] = 0;
+  }
+
+  buff_front = samples;
+  baseRead = buff_front;
+  readIndex = baseRead;
+  stabilized = false;
+}
+
+void display_stabilization_screen() {
+  tft.fillScreen(ILI9341_WHITE);
+  tft.setRotation(3);
+  tft.setTextSize(3);
+  tft.setCursor(40, 100);
+  tft.setTextColor(ILI9341_BLACK);
+  tft.println("Stabilizing...");
+  tft.setRotation(2);
+}
+
+void drawColumn() {
+  // If the buff_front causes readIndex to be behind the front of the buffer,
+  // wrap it around from the end of the buffer
+  if (readIndex < samples) {
+    readIndex = (samples + SAMPLES_TO_DRAW) - (samples - readIndex);
+  }
+
+  // Get the sample
+  int sample = *readIndex;
+  // Get the difference from the baseline
+  int differenceFromBase = sample - BASELINE;
+  // Turn this into pixels
+  int normalizedDiff = abs(differenceFromBase) / (float)HALF_MAX * 120;
+  // Subtract or add it onto the pixel level of the baseline
+  int height = 120;
+  if (differenceFromBase < 0) height -= normalizedDiff;
+  else height += normalizedDiff;
+
+  // The current number of samples we've read since the baseRead
+  int samplesRead = SAMPLES_TO_DRAW - ((baseRead + SAMPLES_TO_DRAW) - readIndex);
+
+  // Draw the grid
+  if (VLinePositions[samplesRead])
+    tft.fillRect(0, samplesRead, 240, 1, ILI9341_RED);
+  else
+    tft.fillRect(0, samplesRead, 240, 1, ILI9341_WHITE);
+  for (int i = 0; i < NUM_HLINES; i++) {
+    tft.drawPixel(HLineHeights[i], samplesRead, ILI9341_RED);
+  }
+  // Draw the graph
+  tft.fillRect(height, samplesRead, 3, 3, ILI9341_BLACK);
+
+  // Advance the readIndex
+  readIndex++;
+  // If we'd read X amount past the base, advance the base
+  // Basically so we're reading a chunk of data at a time
+  if (readIndex >= baseRead + SAMPLES_TO_DRAW) {
+    baseRead = buff_front - SAMPLES_TO_DRAW;
+    readIndex = baseRead;
+  }
+
+  // delay(4);
+}
 
 /*
 	ADC_CFG1_ADIV(2)         Divide ratio = 4 (F_BUS = 48 MHz => ADCK = 12 MHz)
@@ -243,7 +305,6 @@ void adcCalibrate() {
 // which is 250.
 #define PDB_PERIOD (F_BUS / 128 / 10 / SAMPLE_RATE)
 
-
 void pdbInit() {
 
 	// Enable PDB clock
@@ -280,14 +341,14 @@ void dmaInit() {
 	// Destination offset (2 byte)
 	DMA_TCD1_DOFF = 2;
 	// Restore destination address after major loop
-	DMA_TCD1_DLASTSGA = -sizeof(samples);
+	DMA_TCD1_DLASTSGA = -sizeof(dma_buffer);
 	// Source and destination size 16 bit
 	DMA_TCD1_ATTR = DMA_TCD_ATTR_SSIZE(1) | DMA_TCD_ATTR_DSIZE(1);
 	// Number of bytes to transfer (in each service request)
 	DMA_TCD1_NBYTES_MLNO = 2;
 	// Set loop counts
-	DMA_TCD1_CITER_ELINKNO = sizeof(samples) / 2;
-	DMA_TCD1_BITER_ELINKNO = sizeof(samples) / 2;
+	DMA_TCD1_CITER_ELINKNO = sizeof(dma_buffer) / 2;
+	DMA_TCD1_BITER_ELINKNO = sizeof(dma_buffer) / 2;
 	// Enable interrupt (end-of-major loop)
 	DMA_TCD1_CSR = DMA_TCD_CSR_INTMAJOR;
 
@@ -302,10 +363,6 @@ void dmaInit() {
 	NVIC_ENABLE_IRQ(IRQ_DMA_CH1);
 }
 
-
-// remember to set flags so we can tell when a dma has occurred
-
-
 void adc0_isr() {
 }
 
@@ -316,11 +373,21 @@ void pdb_isr() {
 
 void dma_ch1_isr() {
   NVIC_DISABLE_IRQ(IRQ_DMA_CH1);
-  for (int i = 0; i < NUM_DMA_SAMPLES; i++ && buff_front++) {
+
+  for (int i = 0; i < NUM_DMA_SAMPLES; i++) {
     *buff_front = dma_buffer[i];
+    buff_front++;
+    // Stabilize after getting 320 samples
+    if (buff_front > (samples + 640)) stabilized = true;
+    // Use the buffer as a circular buffer
+    if (buff_front > (samples + NUM_SAMPLES)) {
+      buff_front = samples;
+      writeToSDCard = true;
+    }
   }
   // Clear interrupt request for channel 1
   DMA_CINT = 1;
   dma_occurred = true;
+
   NVIC_ENABLE_IRQ(IRQ_DMA_CH1);
 }
